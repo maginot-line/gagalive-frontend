cfd4de90162a0ed441e80fbedefe8931fe7a3148
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-30 09:55:48 +0900

    1.2 Router Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
