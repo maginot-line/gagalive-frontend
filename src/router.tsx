938276d12fafb2396334cfda549a62071acec857
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-30 10:53:03 +0900

    1.3 Not Found Page

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
-import Root from "./components/Root";
-import Home from "./routes/Home";
+import { createBrowserRouter } from 'react-router-dom';
+import Root from './components/Root';
+import Home from './routes/Home';
+import NotFound from './routes/NotFound';
 
-const router = createBrowserRouter([{ path: "/", element: <Root />, children: [{ path: "", element: <Home /> }] }]);
+const router = createBrowserRouter([{ path: '/', element: <Root />, errorElement: <NotFound />, children: [{ path: '', element: <Home /> }] }]);
 
 export default router;
