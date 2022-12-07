d4e3a09c25847d7e89b6bfe28839248908059cc0
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-12-07 11:21:46 +0900

    2.6 Responsive Design

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Grid, GridItem, HStack, Image, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { GiIceBomb } from 'react-icons/gi';
 import io from 'socket.io-client';
 
 interface IForm {
@@ -38,6 +39,9 @@ export default function Home() {
   };
   const leaveRoom = () => {
     socket.emit('leave_room', roomId, () => {});
+    const roomBoxElement = document.getElementById('roomBoxElement') as HTMLElement;
+    const messageDiv = roomBoxElement.querySelectorAll('div');
+    messageDiv.forEach((e) => e.remove());
     setIsEnterRoom(false);
   };
   const onSubmit = (data: IForm) => {
@@ -59,24 +63,67 @@ export default function Home() {
   socket.on('break_room', () => {
     leaveRoom();
   });
+  const timer = (i: string) => {
+    const timerElement = document.getElementById(`timer${i}`) as HTMLElement;
+    let time = 60;
+    const timer = setInterval(() => {
+      time--;
+      timerElement.innerText = time.toString();
+      if (time === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  };
+  const [width, setWidth] = useState(window.innerWidth);
+  window.addEventListener('resize', () => {
+    setWidth(window.innerWidth);
+  });
   return (
     <HStack justifyContent='center'>
-      <Grid h='85vh' w='container.lg' mt={10} columnGap={4} templateRows='repeat(2, 1fr)' templateColumns='repeat(6, 1fr)'>
-        <GridItem colSpan={4}>
-          <Grid rowGap={4} columnGap={4} templateColumns='repeat(2, 1fr)'>
-            {[1, 2, 3, 4, 5, 6].map((item) => (
-              <GridItem colSpan={1} key={item}>
-                <Image
-                  h='250'
-                  borderRadius={'lg'}
-                  src='https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720'
-                />
-              </GridItem>
-            ))}
+      <Grid maxH='85vh' w='container.lg' mt={10} columnGap={4} templateRows='repeat(2, 1fr)' templateColumns='repeat(6, 1fr)'>
+        <GridItem colSpan={{ sm: 3, md: 4 }}>
+          <Grid rowGap={4} columnGap={4} templateColumns={{ sm: '1fr', md: 'repeat(2, 1fr)' }}>
+            {width <= 640 ? (
+              <>
+                {[0, 1, 2].map((_, i) => (
+                  <GridItem position='relative' colSpan={1} key={i}>
+                    <Image
+                      h='250'
+                      borderRadius={'lg'}
+                      src='https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720'
+                    />
+                    <Button onClick={() => timer(i.toString())} variant='unstyled' position='absolute' top={0} right={1} color='white'>
+                      <HStack justifyContent='start' spacing={1}>
+                        <GiIceBomb size='20px' />
+                        <p id={'timer' + i}>60</p>
+                      </HStack>
+                    </Button>
+                  </GridItem>
+                ))}
+              </>
+            ) : (
+              <>
+                {[0, 1, 2, 3, 4, 5].map((_, i) => (
+                  <GridItem position='relative' colSpan={1} key={i}>
+                    <Image
+                      h='250'
+                      borderRadius={'lg'}
+                      src='https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720'
+                    />
+                    <Button onClick={() => timer(i.toString())} variant='unstyled' position='absolute' top={0} right={1} color='white'>
+                      <HStack justifyContent='start' spacing={1}>
+                        <GiIceBomb size='20px' />
+                        <p id={'timer' + i}>60</p>
+                      </HStack>
+                    </Button>
+                  </GridItem>
+                ))}
+              </>
+            )}
           </Grid>
         </GridItem>
-        <GridItem rowSpan={2} colSpan={2}>
-          <Box id='roomBoxElement' position='relative' h='722' borderWidth={1} borderRadius='lg' paddingTop={'12'} paddingBottom={4} px='4'>
+        <GridItem rowSpan={2} colSpan={{ sm: 3, md: 2 }}>
+          <Box id='roomBoxElement' position='relative' h='90%' borderWidth={1} borderRadius='lg' paddingTop={'12'} paddingBottom={4} px='4'>
             {isEnterRoom ? (
               <Button onClick={leaveRoom} position='absolute' top={0} right={0}>
                 Leave Room
