b5e9279113f66b494ddc88c2c9f83fe42f2a11b7
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-12-13 16:27:59 +0900

    2.7 Concurrent Users

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, GridItem, HStack, Image, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { Box, Button, Grid, GridItem, HStack, Image, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { GiIceBomb } from 'react-icons/gi';
@@ -12,60 +12,73 @@ let roomId: string;
 let socketId: string;
 
 export default function Home() {
-  const socket = io('http://127.0.0.1:8000');
+  const socket = io('http://127.0.0.1:8000', { transports: ['websocket'] });
   const [isEnterRoom, setIsEnterRoom] = useState(false);
   const { register, handleSubmit, reset } = useForm<IForm>();
   const addMessage = (message: string, justifyContent: string) => {
-    const roomBoxElement = document.getElementById('roomBoxElement') as HTMLElement;
+    const chatElement = document.getElementById('chatElement') as HTMLElement;
     const messageDiv = document.createElement('div');
     messageDiv.style.display = 'flex';
     messageDiv.style.justifyContent = justifyContent;
-    messageDiv.style.marginTop = '0.5rem';
+    messageDiv.style.marginTop = '0.25rem';
     const messageP = document.createElement('span');
     messageP.style.borderRadius = '0.375rem';
     messageP.style.borderWidth = '1px';
     messageP.style.borderColor = 'gray.200';
-    messageP.style.padding = '0.5rem';
+    messageP.style.padding = '0.25rem';
+    messageP.style.wordBreak = 'break-all';
     messageP.innerText = message;
     messageDiv.appendChild(messageP);
-    roomBoxElement.appendChild(messageDiv);
+    chatElement.appendChild(messageDiv);
   };
   socket.on('connect', () => {
     socketId = socket.id;
+  });
+  const [concurrentUsers, setConcurrentUsers] = useState(0);
+  socket.on('concurrent_users', (users: number) => {
+    setConcurrentUsers(users);
   });
   const joinRoom = () => {
     socket.emit('join_room', () => {});
     setIsEnterRoom(true);
   };
   const leaveRoom = () => {
-    socket.emit('leave_room', roomId, () => {});
-    const roomBoxElement = document.getElementById('roomBoxElement') as HTMLElement;
-    const messageDiv = roomBoxElement.querySelectorAll('div');
+    socket.emit('leave_room', roomId, socketId, () => {});
+    const chatElement = document.getElementById('chatElement') as HTMLElement;
+    const messageDiv = chatElement.querySelectorAll('div');
     messageDiv.forEach((e) => e.remove());
     setIsEnterRoom(false);
   };
   const onSubmit = (data: IForm) => {
+    if (data.message === '') {
+      return;
+    }
     socket.emit('send_message', roomId, socketId, data.message, () => {});
     reset();
   };
-  socket.on('welcome', (data) => {
-    roomId = data;
-    addMessage('Welcome!', 'start');
+  socket.on('welcome', (type: string, receiveRoomId: string) => {
+    roomId = receiveRoomId;
+    if (type === 'join') {
+      addMessage('Welcome!', 'start');
+    }
   });
-  socket.on('receive_message', (receiveId: string, message: string) => {
-    if (socketId === receiveId) {
+  socket.on('receive_message', (receiveSocketId: string, message: string) => {
+    if (socketId === receiveSocketId) {
       addMessage(message, 'end');
-      return;
     } else {
       addMessage(message, 'start');
     }
+    const chatElement = document.getElementById('chatElement') as HTMLElement;
+    chatElement.scrollTop = chatElement.scrollHeight;
   });
-  socket.on('break_room', () => {
-    leaveRoom();
+  socket.on('break_room', (receiveSocketId) => {
+    if (socketId !== receiveSocketId) {
+      leaveRoom();
+    }
   });
   const timer = (i: string) => {
-    const timerElement = document.getElementById(`timer${i}`) as HTMLElement;
     let time = 60;
+    const timerElement = document.getElementById(`timer${i}`) as HTMLElement;
     const timer = setInterval(() => {
       time--;
       timerElement.innerText = time.toString();
@@ -80,71 +93,61 @@ export default function Home() {
   });
   return (
     <HStack justifyContent='center'>
-      <Grid maxH='85vh' w='container.lg' mt={10} columnGap={4} templateRows='repeat(2, 1fr)' templateColumns='repeat(6, 1fr)'>
-        <GridItem colSpan={{ sm: 3, md: 4 }}>
-          <Grid rowGap={4} columnGap={4} templateColumns={{ sm: '1fr', md: 'repeat(2, 1fr)' }}>
-            {width <= 640 ? (
-              <>
-                {[0, 1, 2].map((_, i) => (
-                  <GridItem position='relative' colSpan={1} key={i}>
-                    <Image
-                      h='250'
-                      borderRadius={'lg'}
-                      src='https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720'
-                    />
-                    <Button onClick={() => timer(i.toString())} variant='unstyled' position='absolute' top={0} right={1} color='white'>
-                      <HStack justifyContent='start' spacing={1}>
-                        <GiIceBomb size='20px' />
-                        <p id={'timer' + i}>60</p>
-                      </HStack>
-                    </Button>
-                  </GridItem>
-                ))}
-              </>
-            ) : (
-              <>
-                {[0, 1, 2, 3, 4, 5].map((_, i) => (
-                  <GridItem position='relative' colSpan={1} key={i}>
-                    <Image
-                      h='250'
-                      borderRadius={'lg'}
-                      src='https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720'
-                    />
-                    <Button onClick={() => timer(i.toString())} variant='unstyled' position='absolute' top={0} right={1} color='white'>
-                      <HStack justifyContent='start' spacing={1}>
-                        <GiIceBomb size='20px' />
-                        <p id={'timer' + i}>60</p>
-                      </HStack>
-                    </Button>
-                  </GridItem>
-                ))}
-              </>
-            )}
-          </Grid>
-        </GridItem>
-        <GridItem rowSpan={2} colSpan={{ sm: 3, md: 2 }}>
-          <Box id='roomBoxElement' position='relative' h='90%' borderWidth={1} borderRadius='lg' paddingTop={'12'} paddingBottom={4} px='4'>
-            {isEnterRoom ? (
-              <Button onClick={leaveRoom} position='absolute' top={0} right={0}>
-                Leave Room
-              </Button>
-            ) : (
-              <Button onClick={joinRoom} position='absolute' top={0} right={0}>
-                Enter Room
-              </Button>
-            )}
-          </Box>
-          <Box as='form' onSubmit={handleSubmit(onSubmit)}>
-            <InputGroup mt={4} size='md'>
-              <Input {...register('message')} pr='4.5rem' variant='filled' placeholder='Enter your message' />
-              <InputRightElement w='4.5rem'>
-                <Button type='submit' h='1.75rem' size='sm' variant='solid'>
-                  Send
+      <Grid w='container.xl' mt={10} gap={2} templateRows='repeat(3, 1fr)' templateColumns={width > 640 ? 'repeat(4, 1fr)' : 'repeat(3, 1fr)'}>
+        <GridItem display='flex' flexDirection='column' rowSpan={3} colSpan={2}>
+          <Box
+            id='chatElement'
+            overflowY='auto'
+            flex='1 1 auto'
+            height={0}
+            borderWidth={1}
+            borderRadius='lg'
+            p={4}
+            sx={{
+              '&::-webkit-scrollbar': {
+                display: 'none',
+              },
+            }}
+          ></Box>
+          <Box id='buttonElement' mt={2}>
+            <HStack spacing='4' justifyContent='end'>
+              <Text>{concurrentUsers} connected</Text>
+              {isEnterRoom ? (
+                <Button onClick={leaveRoom} size='sm'>
+                  Leave Room
                 </Button>
-              </InputRightElement>
-            </InputGroup>
+              ) : (
+                <Button onClick={joinRoom} size='sm'>
+                  Enter Room
+                </Button>
+              )}
+            </HStack>
+            <Box as='form' onSubmit={handleSubmit(onSubmit)}>
+              <InputGroup mt={2} size='md'>
+                <Input {...register('message')} pr='4.5rem' variant='filled' placeholder='Enter your message' />
+                <InputRightElement w='4.5rem'>
+                  <Button type='submit' h='1.75rem' size='sm' variant='solid'>
+                    Send
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Box>
           </Box>
         </GridItem>
+        {(width > 640 ? [0, 1, 2, 3, 4, 5] : [0, 1, 2]).map((_, i) => (
+          <GridItem position='relative' rowSpan={1} colSpan={1} key={i}>
+            <Image
+              borderRadius={'lg'}
+              src='https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720'
+            />
+            <Button onClick={() => timer(i.toString())} variant='unstyled' position='absolute' top={0} right={1} color='white'>
+              <HStack justifyContent='start' spacing={1}>
+                <GiIceBomb size='20px' />
+                <p id={'timer' + i}>60</p>
+              </HStack>
+            </Button>
+          </GridItem>
+        ))}
       </Grid>
     </HStack>
   );
